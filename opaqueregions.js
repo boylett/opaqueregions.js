f439f20fadf0eff59caa02a70453eaea17b22492
Boylett <boylett@users.noreply.github.com>
2015-11-26 12:39:53 +0000

    Update opaqueregions.js
    Added collision detection and merging

--- opaqueregions.js
+++ opaqueregions.js
@@ -1,6 +1,6 @@
 function opaqueregions(source, x, y, width, height)
 {
-	var canvas, context, pixels, boxes = [];
+	var canvas, context, pixels, boxes = [], collissions = [];
 	
 	// If image data was provided, we don't need to retrieve it from the canvas
 	if(source instanceof ImageData)
@@ -56,21 +56,89 @@ function opaqueregions(source, x, y, width, height)
 			for(var x = boxes[i].left; x < canvas.width; x ++)
 			{
 				// If the pixel is valid and transparent, set the 'right' value of the box
-				if(x > 0 && (pixels.data[(((y * canvas.width) + x) * 4) + 3] == 0 || x >= (canvas.width - 1)) && right === null)
+				if(x > 0 && (pixels.data[(((y * canvas.width) + x) * 4) + 3] == 0 || x == (canvas.width - 1)) && right === null)
 				{
-					right = x;
+					right = (x == (canvas.width - 1)) ? x + 1 : x;
 				}
 			}
 			
 			// If the pixel is valid and transparent, set the 'bottom' value of the box
-			if(y > 0 && (pixels.data[(((y * canvas.width) + boxes[i].left) * 4) + 3] == 0 || y >= (canvas.height - 1)) && bottom === null)
+			if(y > 0 && (pixels.data[(((y * canvas.width) + boxes[i].left) * 4) + 3] == 0 || y == (canvas.height - 1)) && bottom === null)
 			{
-				bottom = y;
+				bottom = (y == (canvas.height - 1)) ? y + 1 : y;
 			}
 		}
 		
 		boxes[i].right = right;
 		boxes[i].bottom = bottom;
+	}
+	
+	// Search for colliding boxes and mark them
+	for(var i in boxes)
+	{
+		for(var j in boxes)
+		{
+			if(
+				j != i &&
+				boxes[j].left < boxes[i].left + (boxes[i].right - boxes[i].left) &&
+				boxes[j].left + (boxes[j].right - boxes[j].left) > boxes[i].left &&
+				boxes[j].top < boxes[i].top + (boxes[i].bottom - boxes[i].top) &&
+				(boxes[j].bottom - boxes[j].top) + boxes[j].top > boxes[i].left
+			)
+			{
+				var collission = [i, j];
+					collission.sort();
+					collission = collission.join(',');
+				
+				if(collissions.indexOf(collission) == -1)
+				{
+					collissions.push(collission);
+				}
+			}
+		}
+	}
+	
+	// For each collission, measure the minimum and maximum bounds in the colliding area and merge them into a single box
+	for(var i in collissions)
+	{
+		var collission = collissions[i].split(','),
+			mintop = canvas.height,
+			maxright = 0,
+			maxbottom = 0,
+			minleft = canvas.width;
+		
+		for(var j in collission)
+		{
+			if(boxes[collission[j]].top < mintop)
+			{
+				mintop = boxes[collission[j]].top;
+			}
+			
+			if(boxes[collission[j]].right > maxright)
+			{
+				maxright = boxes[collission[j]].right;
+			}
+			
+			if(boxes[collission[j]].bottom > maxbottom)
+			{
+				maxbottom = boxes[collission[j]].bottom;
+			}
+			
+			if(boxes[collission[j]].left < minleft)
+			{
+				minleft = boxes[collission[j]].left;
+			}
+			
+			delete boxes[collission[j]];
+		}
+		
+		boxes.push(
+		{
+			top: mintop,
+			right: maxright,
+			bottom: maxbottom,
+			left: minleft
+		});
 	}
 	
 	// Now we have an array of objects containing various box bounds :) Simples!
